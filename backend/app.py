ece786a28fd790a02e43179228e93559b477ff2b
Manav Krishnaswamy <manavgopalram007@gmail.com>
2025-01-10 14:38:11 -0500

    deployment of website on localhost5000

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +11,22 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app) #creating db
 
-import routes #mounted routes
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+#server static files from dist folder under frontend dir
+
+@app.route('/', defaults={"filename": ""})
+@app.route("/<path:filename>")
+
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
+
+
+#mounted routes
+import routes 
 
 
 with app.app_context():
