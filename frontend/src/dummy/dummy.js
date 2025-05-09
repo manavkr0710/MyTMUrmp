03a2d74c53a95d1d847eec725b3095bdbed2dc63
manavkr0710 <manavgopalram007@gmail.com>
2025-05-09 12:17:37 -0400

    heroku commit

---
+++ frontend/src/dummy/dummy.js
@@ -0,0 +1,55 @@
+export const USERS = [
+	{
+		id: 1,
+		name: "Segun Adebayo",
+		subject: "Creator, Chakra UI",
+		code: "CPS109",
+		description:
+			"With Chakra UI, I wanted to sync the speed of development with the speed of design. I wanted the developer to be just as excited as the designer to create a screen.",
+	},
+	{
+		id: 2,
+		name: "Ryan Florence",
+		subject: "Co-creator, React Router",
+		code: "CPS109",
+		description:
+			"I think the biggest thing that React Router tries to do is to make sure that the URL and the UI are always in sync with each other.",
+	},
+	{
+		id: 3,
+		name: "Kent C. Dodds",
+		subject: "Creator, Testing Library",
+		code: "CPS109",
+		description:
+			"The more your tests resemble the way your software is used, the more confidence they can give you.",
+	},
+	{
+		id: 4,
+		name: "Michael Shilman",
+		subject: "Creator, Playroom",
+		code: "CPS109",
+		description:
+			"With Playroom, you can create a zero-install code-oriented design environment that makes it easy to build and share components.",
+	},
+	{
+		id: 5,
+		name: "Cassidy Williams",
+		subject: "SWE 2",
+		code: "CPS109",
+		description: "I love the web platform and I want to make it as accessible as possible.",
+	},
+	{
+		id: 6,
+		name: "Chris Biscardi",
+		subject: "SWE 3",
+		code: "CPS109",
+		description: "I build things and write about it to help you become a better engineer.",
+	},
+	{
+		id: 7,
+		name: "Emma Bostian",
+		subject: "Senior SWE, Spotify",
+		code: "CPS109",
+		description: "I'm a software engineer with a passion for building scalable web applications.",
+	},
+];
