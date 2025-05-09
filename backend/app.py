d9cb572e10f0a644148460ff308c21e149ff029f
manavkr0710 <manavgopalram007@gmail.com>
2025-05-09 12:30:55 -0400

    Update static file serving and include frontend build

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/dist', static_url_path='')
 CORS(app)
 
 # Use PostgreSQL in production, SQLite in development
@@ -16,27 +16,19 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app) #creating db
 
-frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
-dist_folder = os.path.join(frontend_folder, "dist")
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
 
-#server static files from dist folder under frontend dir
-
-@app.route('/', defaults={"filename": ""})
-@app.route("/<path:filename>")
-
-def index(filename):
-    if not filename:
-        filename = "index.html"
-    return send_from_directory(dist_folder, filename)
-
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
 
 #mounted routes
 import routes 
 
-
 with app.app_context():
     db.create_all() 
-
 
 if __name__ == "__main__":
     app.run(debug=True)
